331758b4c868cbc17406a34d0d5afb50efbf2cf6
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-17 16:12:01 +0200

    Add mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,11 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    field :create_author, mutation: Mutations::CreateAuthor
+    field :update_author, mutation: Mutations::UpdateAuthor
+    field :delete_author, mutation: Mutations::DeleteAuthor
+
+    field :create_book, mutation: Mutations::CreateBook
+    field :update_book, mutation: Mutations::UpdateBook
+    field :delete_book, mutation: Mutations::DeleteBook
   end
 end
