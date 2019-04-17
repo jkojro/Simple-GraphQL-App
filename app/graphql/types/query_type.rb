b759773a6053db75eea43852ce1b87f411d7abe6
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-17 14:02:10 +0200

    Add authors & books queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,8 +8,34 @@ module Types
       description "gets all authors"
     end
 
+    field :author, AuthorType, null: true do
+      description "gets author by id"
+      argument :id, ID, required: true
+    end
+
+    field :books, [BookType], null: true do
+      description "gest all books"
+    end
+
+    field :book, BookType, null: true do
+      description "gets bookmby id"
+      argument :id, ID, required: true
+    end
+
     def authors
       Author.all
+    end
+
+    def author(id:)
+      Author.find(id)
+    end
+
+    def books
+      Book.all
+    end
+
+    def book(id:)
+      Book.find(id)
     end
   end
 end
