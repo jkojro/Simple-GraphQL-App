8b53b0ea19a7f49bd2dacca9b46221b1daa972a6
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-17 12:20:44 +0200

    Add graphiql gem

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     #API ROUTES SHOULD GO HERE
   end
 
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
   #Do not place any routes below this one
   if Rails.env.production?
     get '*other', to: 'static#index'
